a94401904db62cd6397ff3c51f7d346f6a1b18e7
Mark Hatch <58341254+mark2382@users.noreply.github.com>
2026-05-12 13:04:41 -0700

    Added documentation to timer0

--- i2cmeter2/nextion ux documentation/bandScan/TM0.c
+++ i2cmeter2/nextion ux documentation/bandScan/TM0.c
@@ -48,15 +48,15 @@ if(nNowState.val>100)
 {
   if(pm.sh.txt!="J")
   {
-    nRecvCount.val--
+    nRecvCount.val--                // default is to run the scan 3x for each band
     sys2=0
-    for(sys0=1;sys0<120;sys0++)
+    for(sys0=1;sys0<120;sys0++)     //sys0 is x position on graph. There are 120 values
     {
-      substr pm.sh.txt,sTemp0.txt,sys0*2,2
-      covx sTemp0.txt,sys1,2,2
+      substr pm.sh.txt,sTemp0.txt,sys0*2,2      // pull off next 2 bytes of data and place them in sTemp0.txt (hex characters)
+      covx sTemp0.txt,sys1,2,2                  // Convert the hex characters into a 2 byte hex integer (i.e. 0x35, 0xff, etc) in sys1
       //if(sys2>sys1)	//
-      sys2=sys1-sys2
-      if(sys2<0)
+      sys2=sys1-sys2                            // apply offset to sys1 to get sys2
+      if(sys2<0)                                // keeps sys2 between 0 and 50
       {
         sys2=0
       }
@@ -64,10 +64,11 @@ if(nNowState.val>100)
       {
         sys2=50
       }
-      nTemp0.val=sys2/2
+      nTemp0.val=sys2/2                         // Divide offset sys2 by 2.  This is probably being done to scale to the size of the graph
       if(nNowState.val==101)	//Band1
       {
-        nTemp0.val=25-nTemp0.val
+        nTemp0.val=25-nTemp0.val                // Since nTemp0.val is being used as the Y height (magnitude) of the scale, need to offset for each scale
+                                                // each scale is 25 hight (hence the divide by 2) and the bottom is 60 appart
       }else if(nNowState.val==102)	//Band2
       {
         nTemp0.val=85-nTemp0.val
@@ -76,8 +77,14 @@ if(nNowState.val>100)
         nTemp0.val=145-nTemp0.val
       }
       //fill sys0*2,nTemp0.val,2,sys2,YELLOW//1500+sys1*2046
-      fill sys0*2,nTemp0.val,2,sys2,YELLOW//1500+sys1*2046
-      if(sys1>nADCDisVal.val)	//maximum adc dccharget
+      fill sys0*2,nTemp0.val,2,sys2,YELLOW//1500+sys1*2046    //sys0*2 is x offset where each bar is 2 wide on x
+                                                              //nTemp0 is the top of the bar, 2 is width
+                                                              //sys2 is the height, but really the bottom. nTemp0.val is the height thinking top down
+      if(sys1>nADCDisVal.val)	//maximum adc dccharget         //Here is where sys2 is adjusted by the ADC discharge constant. Default is 6
+                                                              //So if sys1(remember this is picked out of the ADC data returned by the DSP), is greater than this
+                                                              //ADC discharge, we now set the offset (sys2) = value just read - the discharge rate.
+                                                              //This has the effect of reducing the next value. Effectively damping the effects of spikes.
+                                                              //This is because it takes time for the ADC to discharge its value
       {
         sys2=sys1-nADCDisVal.val
       }
